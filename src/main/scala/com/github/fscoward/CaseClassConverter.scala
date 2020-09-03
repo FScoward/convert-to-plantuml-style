a4ad3bea5c6841aee1f844647374c369e15aa0b7
FScoward <endlick@gmail.com>
2020-09-04 00:30:18 +0900

    class の様々なケースを追加

--- src/main/scala/com/github/fscoward/CaseClassConverter.scala
+++ src/main/scala/com/github/fscoward/CaseClassConverter.scala
@@ -5,13 +5,18 @@ case class PUMLClass(packageName: String, className: String)
 object CaseClassConverter {
   def convertPUMLClass(classStr: String) = {
     val stringList: Array[String] = classStr.split(System.lineSeparator())
-    val packageName = stringList.find(_.matches("^package"))
+    val packageName = readPackageName(stringList)
 
-    PUMLClass("com.github.fscoward", "todo")
+    PUMLClass(packageName.getOrElse(""), "todo")
   }
 
   def readPackageName(source: Array[String]): Option[String] = {
     val r = """(^package) (.*)""".r("package", "name")
+    source.find(s => r.matches(s)).flatMap(r.findFirstMatchIn).map(_.group("name"))
+  }
+
+  def readClassName(source: Array[String]): Option[String] = {
+    val r = """(.*\s|^)(class)\s(.*)(\(.*)""".r("case", "class", "name")
     source.find(s => r.matches(s)).flatMap(r.findFirstMatchIn).map(_.group("name"))
   }
 }
--- src/test/scala/com/github/fscoward/CaseClassConverterSpec.scala
+++ src/test/scala/com/github/fscoward/CaseClassConverterSpec.scala
@@ -1,8 +1,10 @@
 package com.github.fscoward
 
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers._
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
-class CaseClassConverterSpec extends AnyFunSpec {
+class CaseClassConverterSpec extends AnyFunSpec with should.Matchers {
   describe("CaseClassConverter") {
     it("#convert") {
       val sample =
@@ -14,10 +16,10 @@ class CaseClassConverterSpec extends AnyFunSpec {
 
       val actual = CaseClassConverter.convertPUMLClass(sample)
       val expected = PUMLClass("com.github.fscoward", "SampleClass")
-      assert(actual === expected)
+      actual shouldEqual expected
     }
     it("#readPackageName") {
-      val sample ="""package com.github.fscoward
+      val sample = """package com.github.fscoward
                   |
                   |case class SampleClass(name: String)
                   |""".stripMargin
@@ -25,7 +27,49 @@ class CaseClassConverterSpec extends AnyFunSpec {
       val s = sample.split(System.lineSeparator())
       val actual = CaseClassConverter.readPackageName(sample.split(System.lineSeparator()))
       val expected = Some("com.github.fscoward")
-      assert(actual === expected)
+      actual shouldEqual expected
+    }
+    describe("#readClassName") {
+      val testCase = Table(
+        ("case name", "class"),
+        ("case class", "case class"),
+        ("abstract class", "abstract class"),
+        ("sealed class", "sealed class"),
+        ("final class", "final class"),
+        ("protected case name", "class"),
+        ("protected[this] case name", "class")
+      )
+      forAll(testCase) { (caseName, `class`) =>
+        it(caseName) {
+          val sample = s"""package com.github.fscoward
+                      |
+                      |${`class`} SampleClass(name: String)
+                      |""".stripMargin
+
+          val s = sample.split(System.lineSeparator())
+
+          val actual = CaseClassConverter.readClassName(sample.split(System.lineSeparator()))
+          val expected = Some("SampleClass")
+          actual shouldEqual expected
+        }
+      }
+
+      it("コメントアウトされている") {
+        val sample = """package com.github.fscoward
+        |//class Sample(name: String)
+        |// class Sample(name: String)
+        |/* class Sample(name: String) */
+        |/* 
+        | * class Sample(name: String)
+        |**/
+        |class Amazing(naem: String)
+        |""".stripMargin
+        val s = sample.split(System.lineSeparator())
+
+        val actual = CaseClassConverter.readClassName(sample.split(System.lineSeparator()))
+        val expected = Some("Amazing")
+        actual shouldEqual expected
+      }
     }
   }
 }
